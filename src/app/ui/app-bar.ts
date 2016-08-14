fa15e632bd7ec2e23b9cc280c331581d9dc816b4
Marco Berardini <mberardini@evolution-man.com>
2016-08-14 11:30:37 -0700

    [client] Scaffolded app with Main container

---
+++ src/app/ui/app-bar.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector:'app-bar',
+  template: `
+  <header class="app-bar row middle-xs">
+    <span class="logo col-xs-10">
+      Retain
+    </span>
+    <nav class="col-xs-2">
+    </nav>
+  </header>
+  `
+
+})
+export class AppBar {}
